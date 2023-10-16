61af728bc2142fee990af154f4de65fdf30f3159
KIMJEONGSU <wjdtn8536@naver.com>
2023-10-16 19:28:04 +0900

    Time: 2247 ms (5.51%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,10 +1,6 @@
 # Write your MySQL query statement below
-select user_id, case when sum(num)=0 then 0.00
-                     when sum(num)!=0 then round(sum(num)/count(num),2)
-                     when num is null then 0.00
-                     end as confirmation_rate 
-from (select S.user_id , case when C.action='timeout' then 0
-                        when C.action='confirmed' then 1
-                        end as num
-    from Signups S left join Confirmations C on S.user_id=C.user_id) T
-group by user_id
+select B.user_id, ifnull(round(min(A.confirmation_rate)/count(A.confirmation_rate),2),0) as confirmation_rate 
+from (select user_id,
+       sum(if(action='confirmed',1,0)) over (partition by user_id) as confirmation_rate 
+      from Confirmations) A right join Signups B on A.user_id=B.user_id
+group by B.user_id
