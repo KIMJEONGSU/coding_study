23945bfbbeca9f2307f2ed36783ad5ec3993bd62
KIMJEONGSU <wjdtn8536@naver.com>
2023-11-12 16:31:39 +0900

    Time: 803 ms (79.60%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-select player_id, min(event_date) as first_login
+# select player_id, min(event_date) as first_login
+# from Activity
+# group by player_id
+
+select distinct(player_id), min(event_date) over(partition by player_id) as first_login
 from Activity
-group by player_id
